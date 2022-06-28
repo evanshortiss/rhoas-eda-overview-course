51f378f443a1fe6583e5d6b826682ddc5fb25c9c
Evan Shortiss <evanshortiss@gmail.com>
2022-06-27 17:17:31 -0700

    feat: nodejs producer now sends on 2 second intervals

--- nodejs-producer/src/producer.ts
+++ nodejs-producer/src/producer.ts
@@ -1,6 +1,7 @@
 import { KafkaConfig, Kafka } from 'kafkajs'
 import { get } from 'env-var'
 import { songs } from './songs'
+import log from 'barelog'
 
 (async function main () {
   // Read in environment variables and create a KafkaConfig
@@ -21,17 +22,49 @@ import { songs } from './songs'
   const kafka = new Kafka(cfg)
   const producer = kafka.producer()
   
-  console.log('Connecting to Kafka cluster...')
+  log('Connecting to Kafka cluster')
   await producer.connect()
 
-  // Send the predefined list of songs
-  console.log('Sending messages to Kafka cluster...')
-  await producer.send({
-    topic: 'songs',
-    messages: songs
-  })
+  const getRandomSong = () => {
+    const max = songs.length - 1;
+    const idx = Math.floor(Math.random() * max);
 
-  console.log('Done!')
+    return songs[idx]
+  }
 
-  producer.disconnect()
+  const sendSong = async () => {
+    const song = getRandomSong()
+    
+    log(`Sending song "${song.value}" by "${song.key}" to the songs topic in Kafka`)
+    
+    try {
+      await producer.send({
+        topic: 'songs',
+        messages: [song]
+      })
+
+      // Send another song in 2 seconds
+      setTimeout(sendSong, 2000)
+    } catch (e) {
+      log('Error sending song to Kafka:', e)
+    }
+  }
+
+  const exit = () => {
+    log('Disconnecting from Kafka')
+    producer.disconnect()
+      .then(() => process.exit(0))
+      .catch((e) => {
+        log('Failed to gracefully disconnect from Kafka exiting with code 1', e)
+        process.exit(1)
+      })
+  }
+
+  process.on('SIGTERM', exit)
+  process.on('SIGINT', exit)
+
+  log('Start sending messages to Kafka cluster')
+
+  // Sends an initial song, then continues to send one approx every 2 seconds
+  sendSong()
 })()
